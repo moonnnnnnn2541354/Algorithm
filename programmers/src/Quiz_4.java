eabc1b860a7f4e830bd3e8d0cef12c40751b7b7e
tast <ioae7979@gmail.com>
2023-11-01 10:26:27 +0900

    배열 원소의 길이

--- programmers/src/Quiz_4.java
+++ programmers/src/Quiz_4.java
@@ -7,45 +7,58 @@ public class Quiz_4 {
         Scanner scanner = new Scanner(System.in);
 
         //배열
-//        System.out.print("배열 : ");
-//        int[] intArr = {2,3,4,5,7,8,8,8};
-//        for (int i = 0; i < intArr.length; i++) {
-//            System.out.print(intArr[i] + " ");
-//        }
-//        System.out.println();
+        System.out.print("intArr : ");
+        int[] intArr = {2,3,4,5,7,8,8,8};
+        for (int i = 0; i < intArr.length; i++) {
+            System.out.print(intArr[i] + " ");
+        }
+        System.out.println();
 
         // 선언 & 생성
-        int[] intArr = new int[5];
-        System.out.println("1 ~ 10까지의 수를 5번입력받아서 최빈값 구하기");
-        for (int i = 0; i < 5; i++) {
-            System.out.print((i + 1) + " 번째 입력 : ");
-            intArr[i] = scanner.nextInt();
-        }
+//        int[] intArr = new int[5];
+//        System.out.println("1 ~ 10까지의 수를 5번입력받아서 최빈값 구하기");
+//        for (int i = 0; i < 5; i++) {
+//            System.out.print((i + 1) + " 번째 입력 : ");
+//            intArr[i] = scanner.nextInt();
+//        }
 
 
         // 카운트배열 선언 & 생성
         int[] countArr = new int[11];
         for (int i = 0; i < intArr.length; i++) {
-            int num = intArr[i]; // int = 2;
-            countArr[num] +=1; // 2번째 인덱스 자리에 1+1
+            int count = intArr[i];
+            countArr[count]++;
         }
 
         // 카운트 확인 코드
-        System.out.print("카운트 확인 : ");
+        System.out.print("countArr : ");
         for (int i = 0; i < countArr.length; i++) {
             System.out.print(countArr[i] + " ");
         }
         System.out.println();
 
         // 최빈값 찾기
-        int result = 0;
         int max = 0;
+        int index = 0;
         for (int i = 0; i < countArr.length; i++) {
             if (max < countArr[i]) {
                 max = countArr[i];
-                result = i;
+                index = i;
             }
         }
-        System.out.print("카운트 최고값 찾기 : " + result);
+        System.out.println("count max : " + max);
+        System.out.println("arr index : " + index);
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ programmers/src/level0/ArrayDateLength.java
@@ -0,0 +1,14 @@
+package level0;
+
+public class ArrayDateLength {
+    // 배열원소의길이
+    public int[] solution(String[] strlist) {
+        char[] chArr = new char[strlist.length];
+        int[] answer = new int[strlist.length];
+        for (int i = 0; i < strlist.length; i++) {
+            chArr = strlist[i].toCharArray();
+            answer[i] = chArr.length;
+        }
+        return answer;
+    }
+}
