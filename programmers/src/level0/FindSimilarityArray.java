5372d14a0cf32ae21b505c9bf983b3514289f1ca
tast <ioae7979@gmail.com>
2023-11-01 14:21:05 +0900

    배열의 유사도(같은값 찾아서 count)

---
+++ programmers/src/level0/FindSimilarityArray.java
@@ -0,0 +1,16 @@
+package level0;
+
+public class FindSimilarityArray {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for (int i = 0; i < s1.length; i++) {
+            for (int j = 0; j < s2.length; j++) {
+                if (s1[i].equals(s2[j])){
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+    programmers\src\level0\FindSimilarityArray
+}
