8decd679f3bc00d4fcf296aaec7f8ded8a5ecfd3
tast <ioae7979@gmail.com>
2023-11-01 10:46:24 +0900

    짝수 제외시키기

---
+++ programmers/src/level0/EvenNumberExcept.java
@@ -0,0 +1,21 @@
+package level0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EvenNumberExcept {
+    // 짝수 제외시키기
+    public int[] solution(int n) {
+    List<Integer> intList = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+        if (i % 2 != 0) {
+            intList.add(i);
+        }
+    }
+    int[] answer = new int[intList.size()];
+        for (int i = 0; i < intList.size(); i++) {
+        answer[i] = intList.get(i);
+    }
+        return answer;
+    }
+}
