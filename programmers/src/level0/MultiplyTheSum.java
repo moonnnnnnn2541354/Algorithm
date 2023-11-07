706b04f42de7709428aefc471a71244338edbf3a
tast <ioae7979@gmail.com>
2023-11-07 21:02:51 +0900

    배열의 곱 vs 배열의합의ê제곱

---
+++ programmers/src/level0/MultiplyTheSum.java
@@ -0,0 +1,17 @@
+package level0;
+
+import java.util.Arrays;
+
+public class MultiplyTheSum {
+    public int solution(int[] num_list) {
+        int num = 1;
+        int sum = 0;
+        for (int i = 0; i < num_list.length; i++) {
+            num *= num_list[i];
+            sum += num_list[i];
+        }
+
+        int answer = (num > sum*sum) ? 0 : 1;
+        return answer;
+    }
+}
