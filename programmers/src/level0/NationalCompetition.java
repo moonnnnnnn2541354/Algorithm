9e2e018391ff6aa38df4267782a2096bfcb2efb9
tast <ioae7979@gmail.com>
2023-11-08 21:20:09 +0900

    전국 대회 선발 고사

--- programmers/src/level0/NationalCompetition.java
+++ programmers/src/level0/NationalCompetition.java
@@ -3,6 +3,46 @@ package level0;
 import java.util.*;
 
 public class NationalCompetition {
+    static class StudentList {
+        int rank;
+        int index;
+
+        public StudentList (int rank, int index) {
+            this.rank = rank;
+            this.index = index;
+        }
+    }
+    public static void main(String[] args) {
+        int[] rank = {3, 7, 2, 5, 4, 6, 1};
+        boolean[] attendance = {false, true, true, true, true, false, false};
+
+        List<StudentList> studentList = new ArrayList<>();
+        for (int i = 0; i < attendance.length; i++) {
+            if (attendance[i]){
+                studentList.add(new StudentList(rank[i],i));
+            }
+        }
+
+        Collections.sort(studentList, new Comparator<StudentList>() {
+            @Override
+            public int compare(StudentList o1, StudentList o2) {
+                return o1.rank - o2.rank ;
+            }
+        });
+
+        for (StudentList rankList : studentList) {
+            System.out.println(rankList.index);
+        }
+
+        int answer = 0;
+        int num = 0;
+        for (int i = 0; i < 3; i++) {
+            answer = num + studentList.get(i).index; // 20403
+            num = answer * 100; // 20400
+        }
+        System.out.println(answer);
+    }
+
     public int solution(int[] rank, boolean[] attendance) {
         Map<Integer,Integer> map = new HashMap<>();
         for (int i = 0; i < rank.length; i++) {
