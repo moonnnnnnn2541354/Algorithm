f0c39b76baf605e2de444682b1556f8ae7d524f7
tast <ioae7979@gmail.com>
2023-11-08 18:00:47 +0900

    문자열 합치기

---
+++ programmers/src/level0/PasteString.java
@@ -0,0 +1,17 @@
+package level0;
+
+import java.util.Scanner;
+
+public class PasteString {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String a = sc.next();
+//        String b = sc.next();
+
+        String a = "apple";
+        String b = "pen";
+
+        System.out.println(a+b);
+
+    }
+}
