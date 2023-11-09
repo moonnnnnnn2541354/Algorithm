d9edd09a1da3d7fb7d8456c50efd53fedf44b751
tast <ioae7979@gmail.com>
2023-11-09 14:46:56 +0900

    배열의 평균값 구하기

---
+++ programmers/src/level0/FindTheAverage.java
@@ -0,0 +1,14 @@
+package level0;
+
+public class FindTheAverage {
+    public double solution(int[] arr) {
+        double answer = 0.0;
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        return answer/ arr.length;
+        // return Arrays.stream(arr).average().getAsDouble();
+    }
+}
+
+
