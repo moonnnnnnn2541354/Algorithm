631148b19b974423b495d5eafd43f602d24b3312
tast <ioae7979@gmail.com>
2023-11-09 14:57:53 +0900

    약수의 합

--- programmers/src/level0/SumOfDivisors.java
+++ programmers/src/level0/SumOfDivisors.java
@@ -10,16 +10,4 @@ public class SumOfDivisors {
         }
         return answer;
     }
-
-    public static void main(String[] args) {
-        int n = 12;
-
-        int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                answer += i;
-            }
-        }
-        System.out.println(answer);
-    }
 }
