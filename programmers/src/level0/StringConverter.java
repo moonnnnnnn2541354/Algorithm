19f6908490e53fb5659196477f0db5c14547b4cf
tast <ioae7979@gmail.com>
2023-11-10 20:34:30 +0900

    문자열 -> 정수값으로 변환

---
+++ programmers/src/level0/StringConverter.java
@@ -0,0 +1,17 @@
+package level0;
+
+public class StringConverter {
+    public int solution(String s) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s = "-1234";
+
+        int answer = Integer.parseInt(s);
+
+        System.out.println(answer);
+    }
+
+}
