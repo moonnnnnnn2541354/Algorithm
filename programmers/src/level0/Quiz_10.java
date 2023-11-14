fb83955993ee2891a225726f7f0a326e24c14c02
tast <ioae7979@gmail.com>
2023-11-14 21:06:08 +0900

    else if 문 안쓰고 if문 쓰기

---
+++ programmers/src/level0/Quiz_10.java
@@ -0,0 +1,18 @@
+package level0;
+
+public class Quiz_10 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static String getName(int age) {
+        if (age < 20) {
+            return "준영";
+        }
+        if (age > 20) {
+            return "준일";
+        }
+        return "준이";
+    }
+}
