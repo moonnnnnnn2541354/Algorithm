00e7ba72180c2db63771f5956afedfdeac33a880
tast <ioae7979@gmail.com>
2023-11-15 09:21:57 +0900

    하샤드 수

---
+++ programmers/src/level0/HarshadNumber.java
@@ -0,0 +1,14 @@
+package level0;
+
+public class HarshadNumber {
+    public boolean solution(int x) {
+        String strNum = String.valueOf(x);
+        int num = 0;
+        for (int i = 0; i < strNum.length(); i++) {
+            num += Integer.parseInt(strNum.substring(i,i+1));
+        }
+
+        boolean answer = (x%num == 0) ? true : false;
+        return answer;
+    }
+}
