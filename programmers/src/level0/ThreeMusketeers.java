2c59297cdedcecd9ac6d67d5a38636e460695378
tast <ioae7979@gmail.com>
2023-11-21 09:35:32 +0900

    삼총사

---
+++ programmers/src/level0/ThreeMusketeers.java
@@ -0,0 +1,21 @@
+package level0;
+
+public class ThreeMusketeers {
+
+    public int solution(int[] number) {
+        int answer = 0;
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i+1; j < number.length; j++) {
+                for (int k = j+1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        if (answer == 0) {
+            answer = -1;
+        }
+        return answer;
+    }
+}
