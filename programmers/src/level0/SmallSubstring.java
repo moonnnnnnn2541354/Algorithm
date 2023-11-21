83eb5e725be4421ad0d0294d2fb3f5d8a218fa5e
tast <ioae7979@gmail.com>
2023-11-21 12:12:36 +0900

    크기가 작은 부분 문자열

---
+++ programmers/src/level0/SmallSubstring.java
@@ -0,0 +1,12 @@
+package level0;
+
+public class SmallSubstring {
+    public int solution(String t, String p) {
+        int answer = 0;
+        for (int i = 0; i < t.length() - p.length() + 1; i++) {
+            answer =
+                (Long.parseLong(p) >= Long.parseLong(t.substring(i, i + p.length()))) ? answer + 1 : answer;
+        }
+        return answer;
+    }
+}
