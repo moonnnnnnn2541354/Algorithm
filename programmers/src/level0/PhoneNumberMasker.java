7886bee30a9e8709205e2c07a5e0d770b2126dee
tast <ioae7979@gmail.com>
2023-11-23 14:42:54 +0900

    핸드폰 번호 가리기

---
+++ programmers/src/level0/PhoneNumberMasker.java
@@ -0,0 +1,11 @@
+package level0;
+
+public class PhoneNumberMasker {
+    public String solution(String phone_number) {
+        StringBuilder answer = new StringBuilder(phone_number);
+        for (int i = 0; i < answer.length()-4; i++) {
+            answer.setCharAt(i,'*');
+        }
+        return answer.toString();
+    }
+}
