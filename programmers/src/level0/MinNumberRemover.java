ac6ef53ef3a317202d17e83cb54df825563b1660
tast <ioae7979@gmail.com>
2023-11-24 10:34:05 +0900

    제일 작은 수 제거하기

--- programmers/src/level0/MinNumberRemover.java
+++ programmers/src/level0/MinNumberRemover.java
@@ -5,8 +5,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class MinNumberRemover {
+
     public int[] solution(int[] arr) {
-        if (arr.length==1){
+        if (arr.length == 1) {
             int[] answer = {-1};
             return answer;
         }
@@ -17,38 +18,15 @@ public class MinNumberRemover {
         Collections.sort(intList);
         int min = intList.get(0);
 
-        int[] answer = new int[arr.length-1];
+        int[] answer = new int[arr.length - 1];
+        int index = 0;
         for (int i = 0; i < arr.length; i++) {
             int num = arr[i];
-            if (num>min){
-                answer[i] = num;
+            if (num > min) {
+                answer[index++] = num;
             }
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {4,3,2,1};
-        if (arr.length==1){
-            int[] answer = {-1};
-            return answer;
-        }
-        List<Integer> intList = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            intList.add(arr[i]);
-        }
-        Collections.sort(intList);
-        int min = intList.get(0);
-
-        int[] answer = new int[arr.length-1];
-        for (int i = 0; i < arr.length; i++) {
-            int num = arr[i];
-            if (num>min){
-                answer[i] = num;
-            }
-        }
-        return answer;
-
     }
 
 }
