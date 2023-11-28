d207636f5fb4f8c11781d9006054aecc6645d5b9
tast <ioae7979@gmail.com>
2023-11-28 20:42:02 +0900

    명예의전당

---
+++ programmers/src/level0/HallOfFame.java
@@ -0,0 +1,29 @@
+package level0;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class HallOfFame {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        List<Integer> intList = new ArrayList<>();
+        for (int i = 0; i < score.length; i++) {
+            if (i < k) {
+                intList.add(score[i]);
+            }
+            if (i >= k) {
+                if (score[i] > intList.get(0)) {
+                    intList.add(score[i]);
+                }
+            }
+            Collections.sort(intList);
+            if (intList.size() > k) {
+                intList.remove(0);
+            }
+            answer[i] = intList.get(0);
+        }
+        return answer;
+    }
+
+}
