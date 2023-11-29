2a729022e529da2a6a8df229ec342b85e84430d0
tast <ioae7979@gmail.com>
2023-11-29 09:27:25 +0900

    수박

---
+++ programmers/src/level0/Watermelon.java
@@ -0,0 +1,13 @@
+package level0;
+
+public class Watermelon {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("수");
+        for (int i = 1; i < n; i++) {
+            sb.append((i%2==0)?"수":"박");
+        }
+        String answer = sb.toString();
+        return answer;
+    }
+}
