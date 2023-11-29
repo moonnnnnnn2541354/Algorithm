a311038a0ca20e2fcade2e29be374e7f5d7abbd8
tast <ioae7979@gmail.com>
2023-11-29 09:13:06 +0900

    가운데 글자 가져오기

---
+++ programmers/src/level0/MiddleLetter.java
@@ -0,0 +1,15 @@
+package level0;
+
+public class MiddleLetter {
+    public String solution(String s) {
+        String answer = "";
+        if (s.length()%2==0){
+            answer = s.substring((s.length()/2)-1,(s.length()/2)+1);
+        }
+        if (s.length()%2!=0){
+            answer = s.substring((s.length()/2),(s.length()/2)+1);
+        }
+        return answer;
+    }
+
+}
