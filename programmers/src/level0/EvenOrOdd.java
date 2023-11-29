2f85023d052b5e969c8ab9d353f84eadb56bba73
tast <ioae7979@gmail.com>
2023-11-29 10:49:49 +0900

    짝수 와 홀수

--- programmers/src/level0/EvenOrOdd.java
+++ programmers/src/level0/EvenOrOdd.java
@@ -1,11 +1,12 @@
 package level0;
 
 public class EvenOrOdd {
+
     public String solution(int num) {
         String answer = "";
-        if (num%2==0){
+        if (num % 2 == 0) {
             answer = "Even";
-        } else if (num%2!=0){
+        } else if (num % 2 != 0) {
             answer = "Odd";
         }
         return answer;
