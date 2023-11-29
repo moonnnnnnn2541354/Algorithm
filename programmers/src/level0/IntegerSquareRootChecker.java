d0243874d143406357c82a397277ee012c145c3c
tast <ioae7979@gmail.com>
2023-11-29 10:51:20 +0900

    정수 제곱근 판별

--- programmers/src/level0/IntegerSquareRootChecker.java
+++ programmers/src/level0/IntegerSquareRootChecker.java
@@ -1,18 +1,19 @@
 package level0;
 
 public class IntegerSquareRootChecker {
+
     public static long solution(long n) {
         long answer = 0;
         long num = 0;
 
-        while (answer == 0){
-            if (num*num == n) {
-                answer = (num+1)*(num+1);
+        while (answer == 0) {
+            if (num * num == n) {
+                answer = (num + 1) * (num + 1);
                 break;
-            } else if (num*num > n) {
+            } else if (num * num > n) {
                 answer = -1;
                 break;
-            } else if (num*num != n) {
+            } else if (num * num != n) {
                 num++;
             }
         }
