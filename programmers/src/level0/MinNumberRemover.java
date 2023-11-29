d18a9fce309bedc19ee99fb8bfbb49354a7e9553
tast <ioae7979@gmail.com>
2023-11-29 10:52:35 +0900

    제일 작은 수 제거하기

--- programmers/src/level0/MinNumberRemover.java
+++ programmers/src/level0/MinNumberRemover.java
@@ -29,4 +29,30 @@ public class MinNumberRemover {
         return answer;
     }
 
+    public static void main(String[] args) {
+        int[] arr = {4,1,2,3};
+        int[] answer = new int[arr.length-1];
+        int minIndex = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[minIndex] > arr[i]){
+                minIndex = i;
+            }
+        }
+        for(int i = minIndex+1; i < arr.length; i++){
+            arr[i-1] = arr[i];
+        }
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = arr[i];
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i <answer.length; i++) {
+            System.out.println(answer[i]);
+        }
+    }
+
 }
