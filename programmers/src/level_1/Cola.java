66815a61a500311c956645e6ce51659b2308cffa
tast <ioae7979@gmail.com>
2023-12-14 09:35:41 +0900

    콜라 문제

---
+++ programmers/src/level_1/Cola.java
@@ -0,0 +1,14 @@
+package level_1;
+
+public class Cola {
+
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while (n >= a) {
+            answer += (n / a * b);
+            n = (n / a * b) + (n % a);
+        }
+        return answer;
+    }
+
+}
