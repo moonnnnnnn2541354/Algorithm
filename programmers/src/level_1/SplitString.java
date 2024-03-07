98de67af7a037f79cb1b8b76dc6b3e11431940d9
dong <ioae7979@gmail.com>
2024-03-07 23:25:22 +0900

    문자열 나누기

---
+++ programmers/src/level_1/SplitString.java
@@ -0,0 +1,24 @@
+package level_1;
+
+public class SplitString {
+    public int solution(String s) {
+        int answer = 1;
+
+        char c = s.charAt(0);
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (count == 0) {
+                answer++;
+                c = s.charAt(i);
+            }
+
+            if (c == s.charAt(i)) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return answer;
+    }
+}
