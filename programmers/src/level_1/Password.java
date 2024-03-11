5bab04a96c8917b3e09d5b0425853931783bb2f2
dong <ioae7979@gmail.com>
2024-03-11 16:25:34 +0900

    둘만의 암호

--- programmers/src/level_1/Password.java
+++ programmers/src/level_1/Password.java
@@ -1,27 +1,32 @@
 package level_1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Password {
     public String solution(String s, String skip, int index) {
-        StringBuilder sbS = new StringBuilder(s);
-        StringBuilder sbSkip = new StringBuilder(skip);
         StringBuilder answer = new StringBuilder();
+
+        StringBuilder sbS = new StringBuilder(s);
+        char[] charArr = skip.toCharArray();
+        List<Character> listSkip = new ArrayList<>();
+        for (char c : charArr) {
+            listSkip.add(c);
+        }
 
         for (int i = 0; i < sbS.length(); i++) {
             char c = sbS.charAt(i);
             for (int j = 0; j < index; j++) {
                 c++;
-                if (c > 122) {
-                    c -= 26;
-                }
-                for (int k = 0; k < sbSkip.length(); k++) {
-                    if (c == sbSkip.charAt(k)) {
+                while (listSkip.contains(c)){
+                    if (listSkip.contains(c)) {
                         c++;
                     }
                     if (c > 122) {
                         c -= 26;
                     }
-
                 }
+
             }
             answer.append(c);
         }
@@ -29,40 +34,48 @@ public class Password {
     }
 
 
-        public static void main (String[]args){
-            String s = "klmnopqrstuvzyx";
-            String skip = "abcdefghij";
-            int index = 20;
-            // 정답 happy
-            // a = 97
+    public static void main(String[] args) {
+        String s = "aukks";
+        String skip = "wbqd";
+        int index = 5;
+        // 정답 happy
+        // a = 97
+        // a(97) ~ z(122) 123부터는 (-26)
 
 
-            StringBuilder sbS = new StringBuilder(s);
-            StringBuilder sbSkip = new StringBuilder(skip);
-            StringBuilder answer = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
 
-            for (int i = 0; i < sbS.length(); i++) {
-                char c = sbS.charAt(i);
-                for (int j = 0; j < index; j++) {
-                    c++;
+        StringBuilder sbS = new StringBuilder(s);
+        char[] charArr = skip.toCharArray();
+        List<Character> listSkip = new ArrayList<>();
+        for (char c : charArr) {
+            listSkip.add(c);
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < sbS.length(); i++) {
+            char c = sbS.charAt(i);
+            for (int j = 0; j < index; j++) {
+                c++;
+                if (c > 122) {
+                    c -= 26;
+                }
+                while (listSkip.contains(c)){
+                    if (listSkip.contains(c)) {
+                        c++;
+                    }
                     if (c > 122) {
                         c -= 26;
                     }
-                    for (int k = 0; k < sbSkip.length(); k++) {
-                        if (c == sbSkip.charAt(k)) {
-                            c++;
-                        }
-                        if (c > 122) {
-                            c -= 26;
-                        }
-
-                    }
                 }
-                answer.append(c);
+
+
             }
-
-            System.out.println(answer);
-
-
+            answer.append(c);
         }
+
+        System.out.println(answer);
+
+
     }
+}
