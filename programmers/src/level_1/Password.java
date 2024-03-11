d70b8aed7568201cfc97caae48f1f61bbd399644
dong <ioae7979@gmail.com>
2024-03-11 16:26:47 +0900

    둘만의 암호

--- programmers/src/level_1/Password.java
+++ programmers/src/level_1/Password.java
@@ -18,7 +18,10 @@ public class Password {
             char c = sbS.charAt(i);
             for (int j = 0; j < index; j++) {
                 c++;
-                while (listSkip.contains(c)){
+                if (c > 122) {
+                    c -= 26;
+                }
+                while (listSkip.contains(c)) {
                     if (listSkip.contains(c)) {
                         c++;
                     }
@@ -26,56 +29,11 @@ public class Password {
                         c -= 26;
                     }
                 }
+
 
             }
             answer.append(c);
         }
         return answer.toString();
-    }
-
-
-    public static void main(String[] args) {
-        String s = "aukks";
-        String skip = "wbqd";
-        int index = 5;
-        // 정답 happy
-        // a = 97
-        // a(97) ~ z(122) 123부터는 (-26)
-
-
-        StringBuilder answer = new StringBuilder();
-
-        StringBuilder sbS = new StringBuilder(s);
-        char[] charArr = skip.toCharArray();
-        List<Character> listSkip = new ArrayList<>();
-        for (char c : charArr) {
-            listSkip.add(c);
-        }
-
-        int cnt = 0;
-        for (int i = 0; i < sbS.length(); i++) {
-            char c = sbS.charAt(i);
-            for (int j = 0; j < index; j++) {
-                c++;
-                if (c > 122) {
-                    c -= 26;
-                }
-                while (listSkip.contains(c)){
-                    if (listSkip.contains(c)) {
-                        c++;
-                    }
-                    if (c > 122) {
-                        c -= 26;
-                    }
-                }
-
-
-            }
-            answer.append(c);
-        }
-
-        System.out.println(answer);
-
-
     }
 }
