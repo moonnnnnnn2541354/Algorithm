92a644f65d585a37af07508afc287b74ea7b902f
dong <ioae7979@gmail.com>
2024-03-13 18:10:27 +0900

    같은 숫자는 싫어

--- programmers/src/level_1/UnequalNumbers.java
+++ programmers/src/level_1/UnequalNumbers.java
@@ -4,17 +4,21 @@ import java.util.Stack;
 
 public class UnequalNumbers {
     public int[] solution(int[] arr) {
-        Stack<Integer> st = new Stack<Integer>();
+        Stack<Integer> st = new Stack<>();
 
         for (int i = 0; i < arr.length; i++) {
-            if (i == 0) st.push(arr[i]);
-            else if (st.peek() != arr[i]) st.push(arr[i]);
+            if (i == 0) {
+                st.push(arr[i]);
+            }
+            if (st.peek() != arr[i]) {
+                st.push(arr[i]);
+            }
         }
+
         int[] answer = new int[st.size()];
         for (int i = st.size() - 1; i >= 0; i--) {
             answer[i] = st.pop();
         }
-
         return answer;
     }
 }
